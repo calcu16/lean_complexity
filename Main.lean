baa9184d535d99b5f2aa423327eb3e01e45c8428
Andrew Carter <acarter@cs.hmc.edu>
2023-09-03 13:33:46 -0700

    Cleaning up

--- Main.lean
+++
@@ -1,4 +0,0 @@
-import Complexity
-
-def main : IO Unit :=
-  IO.println s!"Hello, world!"
--- lakefile.lean
+++ lakefile.lean
@@ -1,5 +1,4 @@
 import Lake
-
 open Lake DSL
 
 package complexity
@@ -7,11 +6,4 @@ package complexity
 @[default_target]
 lean_lib Complexity
 
-@[default_target]
-lean_exe complexity where
-  root := `Main
-
-meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
-
-require std from git "https://github.com/leanprover/std4" @ "main"
+require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
