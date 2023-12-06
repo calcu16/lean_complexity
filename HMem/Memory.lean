31d917feec70a5420c8546da1d949ed1bd1fb2e0
Andrew Carter <acarter@cs.hmc.edu>
2023-12-06 13:29:25 -0800

    builds

--- HMem/Memory.lean
+++ HMem/Memory.lean
@@ -25,8 +25,9 @@ def Setm: Memory α → α → Memory α → Memory α
 | leaf, a, m => node 0 (λ x ↦ ite (x = a) m leaf)
 | node v vs, a, m => node v (λ x ↦ ite (x = a) m (vs x))
 
-theorem Getvp_Setv_nil (m: Memory α) (a: α): Getvp (Setv m a) [] = a :=
-by cases m <;> rfl
+theorem Getvp_Setv_nil: (m: Memory α) → (a: α) → Getvp (Setv m a) [] = a
+| leaf, _ => rfl
+| node _ _, _ => rfl
 
 end Memory
 
--- lakefile.lean
+++ lakefile.lean
@@ -6,4 +6,7 @@ package complexity
 @[default_target]
 lean_lib Complexity
 
+@[default_target]
+lean_lib HMem
+
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
